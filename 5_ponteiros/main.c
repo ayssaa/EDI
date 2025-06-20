5a02843fb04b6ad7cd80ce3d7ba9a0a3039a8c20
Ayssa <rarayssah@gmail.com>
2025-06-20 07:16:27 -0300

    Revisando

--- 5_ponteiros/main.c
+++ 5_ponteiros/main.c
@@ -1,24 +1,23 @@
-#include <stdio.h>
+# include <stdlib.h>
+# include <stdio.h>
 
-void Troca(int *a, int *b) {
-    int temp = *a;  // Guarda o valor apontado por 'a' em uma variável temporária
-    *a = *b;        // Coloca o valor apontado por 'b' no endereço apontado por 'a'
-    *b = temp;      // Coloca o valor temporário no endereço apontado por 'b'
+void Troca(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 void main() {
-    int v1, v2;
+    int n1, n2;
 
-    printf("Digite dois valores: ");
-    scanf("%d %d", &v1, &v2);  // Lê os dois valores e armazena em v1 e v2
+    printf("Digite dois valores:\n");
+    scanf("%d %d", &n1, &n2);
 
-    printf("Antes da troca: valor 1 = %d, valor 2 = %d\n", v1, v2);
+    printf("Valores antes da troca:\n");
+    printf("Valor 1: %d\nValor 2: %d", n1, n2);
 
-    // Declara dois ponteiros que apontam para v1 e v2
-    int *p1 = &v1;
-    int *p2 = &v2;
+    Troca(&n1, &n2);
 
-    Troca(p1, p2);
-
-    printf("Depois da troca: valor 1 = %d, valor 2 = %d\n", v1, v2);
+    printf("\nValores depois da troca:\n");
+    printf("Valor 1: %d\nValor 2: %d", n1, n2);
 }
