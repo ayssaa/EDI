5dbee8bba12665fa7bc573c44dbad9205b13576c
Ayssa <rarayssah@gmail.com>
2025-07-19 20:28:00 -0300

    atv1

---
+++ Exercicios/Lista3/ex01.c
@@ -0,0 +1,2 @@
+// Pilhas e filas são listas encadeadas com regras/restrições específicas, ou seja, elas são iguais listas
+// porém possuem certas restrições em algumas funções, como as de inserção e de remoção
